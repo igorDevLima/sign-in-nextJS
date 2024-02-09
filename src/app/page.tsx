e5f12021a71b6db2ea8ad2c8436c1296bdd65a2a
Igor Lima <igorlimadrum@gmail.com>
2024-02-09 15:11:57 -0300

    feat: add components forms  component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import AddressSignUpForm from "./components/AddressSignUpForm";
+import UserSignUpForm from "./components/UserSignUpForm";
 import styles from "./page.module.sass";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <h1>Sign Up</h1>
+      <AddressSignUpForm />
+      <UserSignUpForm />
+    </main>
+  );
 }
